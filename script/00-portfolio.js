5b84dd26ded456699fefe805c3c4be2a23b2c578
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-10-03 13:41:48 -0300

    Adicionei um switch shadow

--- script/00-portfolio.js
+++ script/00-portfolio.js
@@ -2,10 +2,17 @@ var setorPara = {
    abrirMenu: document.querySelector("#menu-burguer"),
 
    fecharMenu: document.querySelector('#fecha-menu-burguer'),
-   fecharMenuPorFora: document.querySelector('.fecha-menu-por-fora')
+   fecharMenuPorFora: document.querySelector('.fecha-menu-por-fora'),
+
+   alterarBackground: document.querySelector("#button-switch-shadow")
 }
 
 var menu = document.querySelector('.lista-projetos')
+
+filtro = {
+   header: document.querySelector('.comfort-eyes-header'),
+   section: document.querySelector('.comfort-eyes')
+}
 
 setorPara.abrirMenu.addEventListener('click', () => {
    menu.classList.add("active")
@@ -21,3 +28,17 @@ setorPara.fecharMenuPorFora.addEventListener('click', () => {
    menu.classList.remove('active')
    setorPara.fecharMenuPorFora.style.display = 'none'
 })
+
+setorPara.alterarBackground.addEventListener("click", filtroBackground)
+
+function filtroBackground() {
+   if (setorPara.alterarBackground.checked) {
+      filtro.header.style.display = 'block'
+      filtro.section.style.display = 'block'
+   }
+
+   else {
+      filtro.header.style.display = 'none'
+      filtro.section.style.display = 'none'
+   }
+}
